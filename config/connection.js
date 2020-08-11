92b004969f1c9777823a9ff4853a255e8274f4db
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-08-11 00:19:57 -0400

    make the connection

--- config/connection.js
+++ config/connection.js
@@ -8,5 +8,14 @@ if (process.env.JAWSDB_URL) {
     connection = mysqyl.createConnection(process.env.production);
 };
 
+// Make connection.
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
 // Export connection
 module.exports = connection;
