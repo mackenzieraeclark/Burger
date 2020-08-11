efd62105b0143c9df7bac78cb9b262a29d5490ac
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-08-11 00:51:28 -0400

    burger.js begin crud

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,11 @@
 var orm = require("../config/orm.js");
+
+// Use CRUD to call back (cb) the ORM functions
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", function(result) {
+            cb(result);
+        });
+    },
+    create: 
+}
