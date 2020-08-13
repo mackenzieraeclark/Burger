ed586443074ff43168fe5f320909431e5ae76e3e
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-08-13 02:28:56 -0400

    set all routes and export for server

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,3 +3,67 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
+// Create routes
+router.get("/", function(req, res) {
+    res.redirect("/burgers");
+  });
+
+// This route will display all burgers
+router.get("/burgers", function(req, res) {
+    burger.all(function(data) {
+        var burgersData = {
+        burgers: data
+    };
+    console.log(burgersData);
+    res.render("index", burgersData);
+    });
+});
+
+// Using post for api
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+      "burger_name", 
+    ], [
+      req.body.name, 
+    ], function(res) {
+      
+        // Send back ID
+      res.redirect("/burgers");
+    });
+});
+
+// Using put for id
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    //console.log("condition", condition);
+  
+    // Using update
+    burger.update({
+       devoured: "true"
+    }, condition, function(res) {
+      if (res.changedRows == 0) {
+        // 404 for no ID
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+});
+
+// Using delete
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.delete(condition, function(res) {
+      if (res.affectedRows == 0) {
+        // 404 for no ID
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+});
+
+// Export for server
+module.exports = router;
