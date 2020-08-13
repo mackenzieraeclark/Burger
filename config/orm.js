8d437629a4dd5337082c506b86fb7a28967f7437
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-08-13 02:03:44 -0400

    key/value object

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,7 @@
 // mySQL connection
 var connection = require("./connection");
 
-// Create an array for question marks
+// Create function for an array of question marks
 function printQuestionMarks(num) {
     var arr = [];
   
@@ -10,7 +10,30 @@ function printQuestionMarks(num) {
     }
   
     return arr.toString();
-  }
+}
+
+// Function to convert object key-value pairs to the SQL syntax
+function objToSql(ob){
+    var arr = [];
+
+    // Create loop to go through the keys and push string int arr of key/value
+    for (var key in ob){
+        var value = ob[key];
+
+        // Skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)){
+            // Add quotations to strings with spaces
+            if(typeof value === "string" && value.indexOf(" ") >= 0){
+                value = "'" + value + "'";
+            }
+            // push array
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // return the array ---> one comma separated string!
+    return arr.toString();
+}
 
 // Export ORM to model
 module.exports = orm;
