09c990343031ef443ca95889fb9059f8bb767ad8
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-08-13 01:54:49 -0400

    function for question marks

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,16 @@
+// mySQL connection
 var connection = require("./connection");
 
-// `selectAll()`
-// `insertOne()`
-// `updateOne()`
+// Create an array for question marks
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
 
 // Export ORM to model
 module.exports = orm;
